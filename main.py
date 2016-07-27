c222c47a14155d9969c2b88a7932a2fdf19914fd
Rick <rickeason9@gmail.com>
2016-07-26 21:51:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def inf_loop1():
         inputs.start_key_check(settings.key, settings.keyChange)
         # Preceding function sleeps for 0.5
         inf_loop2()
+        inf_loop3()
 
 
 def inf_loop2():
@@ -26,19 +27,19 @@ def inf_loop2():
 
 def inf_loop3():
     # This loop turns on the lights
-    while True:
-        if settings.redSwitch:
-            outputs.light_red_on()
-            print("RED light function on was called")
-        elif not settings.redSwitch:
-            outputs.light_red_off()
-            print("RED light function off was called")
-        elif settings.greenSwitch:
-            outputs.light_red_on()
-            print("GREEN light function on was called")
-        elif not settings.greenSwitch:
-            outputs.light_red_off()
-            print("GREEN light function off was called")
+    # while True:
+    if settings.redSwitch:
+        outputs.light_red_on(settings.key)
+        print("RED light function on was called")
+    elif not settings.redSwitch:
+        outputs.light_red_off(settings.key)
+        print("RED light function off was called")
+    elif settings.greenSwitch:
+        outputs.light_red_on()
+        print("GREEN light function on was called")
+    elif not settings.greenSwitch:
+        outputs.light_red_off()
+        print("GREEN light function off was called")
 
 
 inf_loop1()
--- outputs.py
+++ outputs.py
@@ -2,12 +2,12 @@ import RPi.GPIO
 import Adafruit_GPIO as GPIO
 
 gpio = GPIO.get_platform_gpio()
-gpio.setup(6, GPIO.OUT)  # Color RED output
+gpio.setup(25, GPIO.OUT)  # Color RED output
 
 
 def light_red_on(key):
     if key:
-        gpio.output(6, False)  # Possible inverse of GPIO state.
+        gpio.output(25, True)
         print("RED light set to 100%")
     else:
         print("red_light_on was called but KEY is not on")
@@ -15,7 +15,7 @@ def light_red_on(key):
 
 def light_red_off(key):
     if key:
-        gpio.output(6, True)  # Possible inverse of GPIO state.
+        gpio.output(25, False)
         print("RED light set to 0%")
     else:
         print("red_light_off was called but KEY is not on")
